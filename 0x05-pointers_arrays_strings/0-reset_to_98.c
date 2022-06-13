223d4635f486f5431b4571c265066c184c1ce48d
Ikwuje-peace <peaceikwuje21@gmail.com>
2022-06-13 07:04:18 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - function that takes a pointr to an integer parameter
+* @n: Integer pointer
+* Return: 0 Success
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is thr pointerssection of the low level programming
